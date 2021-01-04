f5a4964e936e4c9954e3d7606527141a4670c086
Christopher Todd <Christopher.Todd@edu.sait.ca>
2021-01-04 09:51:07 -0700

    exercise complete

--- script.js
+++ script.js
@@ -6,12 +6,47 @@ const fs = require('fs');
 
 function question1() {
     fs.readFile('./santa.txt', (err, data) => {
-        const directions = data.toString();
-        console.log(directions)
+        console.time('santa-time'); //how long it takes function to run
+        const directions = data.toString(); //convert text to string
+        const directionsArray = directions.split(''); //split each character into array
+        const answer = directionsArray.reduce((acc, currentValue) => { //where acc = accumulator
+            if (currentValue === '(') {
+                return acc += 1
+            } else if (currentValue === ')') {
+                return acc -= 1
+            }
+        }, 0)
+        console.timeEnd('santa-time');
+        console.log('Final Floor:', answer)
     })
-
 }
 question1();
 
-
 // Question 2: When does Santa first enter the basement?
+
+function question2() {
+    fs.readFile('./santa.txt', (err, data) => {
+        const directions = data.toString();
+        const directionsArray = directions.split('');
+        let accumulator = 0;
+        let counter = 0;
+        const answer = directionsArray.some((currentItem) => { //ends the function when accumulator < 0
+            if (currentItem === '(') {
+                accumulator += 1
+            } else if (currentItem === ')') {
+                accumulator -= 1
+            }
+            counter ++;
+            return accumulator < 0;
+        })
+        console.log('Basement entered on floor:', counter);
+    })
+}
+question2();
+
+
+
+
+
+
+
